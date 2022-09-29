77e4068b8543109c10057c1ae2f0be5bfb516bc9
muskankumari11 <111870054+muskankumari11@users.noreply.github.com>
2022-09-29 19:41:39 +0530

    Create basics.cpp

---
+++ basics.cpp
@@ -0,0 +1,17 @@
+//online compiler
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	for(n=2;n<=18;n++)
+	{
+		if(n%2==0)
+		{
+			cout<<n<<endl;
+		}
+	}
+	// your code goes here
+	return 0;
+}
